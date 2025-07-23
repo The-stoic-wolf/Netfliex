14ea000a17e652f5e0cd9f53d3339b9cd986c041
the-stoic-wolf <1219muhammadishaq@gmail.com>
2025-07-24 03:37:29 +0500

    changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -188,10 +188,10 @@ const visibleimages = images.slice(startindex , startindex + itemsperpage)
 
             <div className="flex h-[300px] w-[90%]  items-center">
              {visibleimages.map((item,index)=>{
-              return <div key={index} className={` ${window.innerWidth <= 576 ? "h-[250px] w-[300px] m-[10px] " : " m-[20px] h-[300px] w-[220px]"} relative bg-gray-300 overflow-visible `}>
+              return <div key={index} className={` ${window.innerWidth <= 576 ? "h-[250px] w-[280px] m-[10px] " : " m-[20px] h-[300px] w-[220px]"} relative bg-gray-300 overflow-visible obj `}>
                   <img className='absolute h-[60px] w-[50px] m-[10px] ' src="netfliex-icon.png" alt="" />
                   <img className="h-full w-full hover:scale-110 object-cover" src={item.img} alt="Sample"/>
-                  <span className={` ${window.innerWidth <= 576 ? "text-[80px] [-webkit-text-stroke:2px_white] font-bold " : " text-[120px] [-webkit-text-stroke:3px_white] font-bold " } absolute left-[-20px] bottom-[-20px]`}>
+                  <span className={` ${window.innerWidth <= 576 ? "text-[80px] [-webkit-text-stroke:2px_white] font-bold left-[-30]" : " text-[120px] [-webkit-text-stroke:3px_white] font-bold left-[-20px]" } absolute  bottom-[-20px]`}>
                     {item.number}
                   </span>
               </div>
